2e20fed61764a87dfe94e7c8e6b03aad2a5b4f8e
nabilladdian <145407227+nabilladdian@users.noreply.github.com>
2024-12-19 11:25:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,84 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'BRICK-1-Boys-001',
+      slug: 'batman-bricks',
+      name: "Batman Bricks",
+      category: 'Boys Bricks',
+      price: 250_000,
+      stock: 50,
+      imageUrl: '/assets/images/batmanbricks.jpg',
+      description: `Bangun dunia superhero dengan Batman Bricks! Set LEGO ini memungkinkan anak-anak untuk menciptakan petualangan seru bersama Batman dan teman-temannya.`
+    },
+    {
+      id: 'BRICK-2-Boys-002',
+      slug: 'superman-bricks',
+      name: "Superman Bricks",
+      category: 'Boys Bricks',
+      price: 250_000,
+      stock: 40,
+      imageUrl: '/assets/images/supermanbricks.jpg',
+      description: `Jadilah pahlawan dengan Superman Bricks! Set LEGO ini memberikan kesempatan untuk membangun berbagai skenario heroik dengan Superman.`
+    },
+    {
+      id: 'BRICK-3-Boys-003',
+      slug: 'thor-bricks',
+      name: "Thor Bricks",
+      category: 'Boys Bricks',
+      price: 300_000,
+      stock: 30,
+      imageUrl: '/assets/images/thorbricks.jpg',
+      description: `Rasakan kekuatan petir dengan Thor Bricks! Set LEGO ini memungkinkan anak-anak untuk membangun petualangan epik bersama Thor dan teman-temannya.`
+    },
+    {
+      id: 'BRICK-4-Boys-004',
+      slug: 'theflash-bricks',
+      name: "The Flash Bricks",
+      category: 'Boys Bricks',
+      price: 250_000,
+      stock: 25,
+      imageUrl: '/assets/images/theflashbricks.jpg',
+      description: `Berlari lebih cepat dari cahaya dengan The Flash Bricks! Set LEGO ini memberikan pengalaman membangun yang menyenangkan dengan karakter The Flash.`
+    },
+    {
+      id: 'BRICK-5-Girls-001',
+      slug: 'elsa-bricks',
+      name: "Elsa Bricks",
+      category: 'Girls Bricks',
+      price: 300_000,
+      stock: 20,
+      imageUrl: '/assets/images/elsabricks.jpg',
+      description: `Ciptakan keajaiban dengan Elsa Bricks! Set LEGO ini memungkinkan anak-anak untuk membangun dunia es yang indah bersama Elsa dari Frozen.`
+    },
+    {
+      id: 'BRICK-6-Girls-002',
+      slug: 'sailormoon-bricks',
+      name: "Sailor Moon Bricks",
+      category: 'Girls Bricks',
+      price: 300_000,
+      stock: 15,
+      imageUrl: '/assets/images/sailormoonbricks.jpg',
+      description: `Bergabunglah dengan petualangan luar angkasa dengan Sailor Moon Bricks! Set LEGO ini membawa karakter Sailor Moon ke dalam dunia kreatif anak-anak.`
+    },
+    {
+      id: 'BRICK-7-Girls-003',
+      slug: 'hellokitty-bricks',
+      name: "Hello Kitty Bricks",
+      category: 'Girls Bricks',
+      price: 250_000,
+      stock: 10,
+      imageUrl: '/assets/images/hellokittybricks.jpg',
+      description: `Buat dunia yang manis dengan Hello Kitty Bricks! Set LEGO ini memberikan kesempatan untuk membangun berbagai skenario lucu dengan Hello Kitty.`
+    },
+    {
+      id: 'BRICK-8-Girls-004',
+      slug: 'kuromi-bricks',
+      name: "Kuromi Bricks",
+      category: 'Girls Bricks',
+      price: 250_000,
+      stock: 5,
+      imageUrl: '/assets/images/kuromibricks.jpg',
+      description: `Jelajahi dunia petualangan dengan Kuromi Bricks! Set LEGO ini memungkinkan anak-anak untuk membangun cerita seru bersama Kuromi.`
+    },
+  ];
+}
